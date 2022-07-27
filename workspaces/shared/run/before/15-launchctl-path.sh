42f4954bf2376db6de5ca6aabd442458d33c6d84
Von Welch <von@vwelch.com>
2022-07-27 09:32:01 -0400

    15-launchctl-path.sh: add ${HOMEBREW_PREFIX}/bin to path

--- workspaces/shared/run/before/15-launchctl-path.sh
+++ workspaces/shared/run/before/15-launchctl-path.sh
@@ -5,10 +5,12 @@
 #
 set -o errexit  # Exit on error
 
-set -x  # DEBUG
 _path="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:$HOME/bin"
+if test -n "${HOMEBREW_PREFIX}" ; then
+  _path="${_path}:${HOMEBREW_PREFIX}/bin"
+fi
 if test "${_path}" != "$(launchctl getenv PATH)" ; then
-  echo "Adding /usr/local/bin and $HOME/bin to default path via launchctl"
+  echo "Setting default path via launchctl: ${_path}"
   echo "Reboot required to take effect."
   sudo launchctl config user path "${_path}"
 fi
