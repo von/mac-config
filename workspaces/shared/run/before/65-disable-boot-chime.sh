6a869fd0e962fae6ecd6b88a2e66f93bf7530425
Von Welch <von@vwelch.com>
2022-07-27 09:32:31 -0400

    65-disable-boot-chime.sh: use StartupMute and check before setting

--- workspaces/shared/run/before/65-disable-boot-chime.sh
+++ workspaces/shared/run/before/65-disable-boot-chime.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-
+# Disable chime on startup
+# Kudos: https://osxdaily.com/2020/04/29/how-enable-startup-boot-sound-newer-mac/
 set -e
 
-echo "Disable chime on boot"
-sudo nvram SystemAudioVolume=" "
+if test $(nvram StartupMute) != "%01" ; then
+  echo "Disable chime on boot"
+  sudo nvram StartupMute=%01
+fi
 
 exit 0
