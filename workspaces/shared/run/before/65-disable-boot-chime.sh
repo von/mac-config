255e65364add2eabbade8900ea3b908140654eac
Von Welch <von@vwelch.com>
2022-09-20 20:37:39 -0400

    65-disable-boot-chime.sh: use hex to detect boot chime being enabled

--- workspaces/shared/run/before/65-disable-boot-chime.sh
+++ workspaces/shared/run/before/65-disable-boot-chime.sh
@@ -3,7 +3,8 @@
 # Kudos: https://osxdaily.com/2020/04/29/how-enable-startup-boot-sound-newer-mac/
 set -e
 
-if test $(nvram StartupMute) != "%01" ; then
+nvram -X StartupMute | grep "0x01" > /dev/null
+if test $? -ne 0 ; then
   echo "Disable chime on boot"
   sudo nvram StartupMute=%01
 fi
