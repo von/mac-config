0d7819aa6ebb1dd72c5e7aa6832ae82c84865ecc
Von Welch <von@vwelch.com>
2022-09-20 20:38:40 -0400

    new file:   workspaces/shared/run/after/80-libdvdcss.sh
    Link /opt/homebrew/lib/libdvdcss to /usr/local/lib where
    HandBrakeCLI looks for it.

---
+++ workspaces/shared/run/after/80-libdvdcss.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# HandBrakeCLI expects libdvdcss to be in /usr/local/lib.
+# If $HOMEBREW_PREFIX is not /usr/local, then create symlinks
+# from $HOMEBREW_PREFIX/lib/ to /usr/local/bin for libdvdcss
+# libraries.
+if test -z "${HOMEBREW_PREFIX}" ; then
+  echo "\$HOMEBREW_PREFIX not defined."
+  exit 1
+fi
+
+if test "${HOMEBREW_PREFIX}" = "/usr/local" ; then
+  # No linking needed
+  exit 0
+fi
+
+for lib in ${HOMEBREW_PREFIX}/lib/libdvdcss* ; do
+  libname=$(basename $lib)
+  target=/usr/local/lib/${libname}
+  if test -f ${target} ; then
+    continue
+  fi
+  echo "Linking ${lib} to ${target}"
+  sudo ln -s ${lib} ${target}
+done
+exit 0
