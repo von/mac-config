df40416cd345e23cc8f952c615c751e3d3cf885b
Von Welch <von@vwelch.com>
2022-11-15 10:49:58 -0500

    70-set-timezone.sh: bug fix for DST

--- workspaces/shared/run/before/70-set-timezone.sh
+++ workspaces/shared/run/before/70-set-timezone.sh
@@ -5,7 +5,8 @@ set -e
 mytimezone="America/Indiana/Indianapolis"
 # Non-sudo test kudos: https://apple.stackexchange.com/a/117995/104604
 #   (See comment from Jon Church)
-if test $(date +"%Z") != "EDT" ; then
+tz=$(date +"%Z")
+if test "$tz" != "EDT" -a "$tz" != "EST" ; then
   echo "Setting timezone to ${mytimezone}"
   sudo systemsetup -settimezone "${mytimezone}"
 fi
