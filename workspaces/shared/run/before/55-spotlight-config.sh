6962a47615a9b2a6b885ec5590ff0504ca43e1e3
Von Welch <von@vwelch.com>
2023-10-21 12:38:00 -0400

    55-spotlight-config.sh: improve parsing of mdutil output

--- workspaces/shared/run/before/55-spotlight-config.sh
+++ workspaces/shared/run/before/55-spotlight-config.sh
@@ -4,11 +4,19 @@
 #
 set -o errexit  # Exit on error
 
-mdutil -s / | grep enabled > /dev/null
-
-if test $? -ne 0 ; then
-  echo "Turning spotlight on for '/'"
-  sudo mdutil -i on /
-fi
+# Check to see if indexing of '/' is enabled
+indexing_status=$(mdutil -s /)
+case "${indexing_status}" in
+  *enabled*)
+    echo "Indexing for / enabled."
+    ;;
+  *unknown*)
+    echo "Turning spotlight on for '/'"
+    sudo mdutil -i on /
+    ;;
+  *)
+    echo "Unrecognized state of indexing for /: ${indexing_status}"
+    ;;
+esac
 
 exit 0
