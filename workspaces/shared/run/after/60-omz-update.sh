c823d9976061a072419dd19b9b5177366fd18427
Von Welch <von@vwelch.com>
2025-07-28 21:55:02 -0400

    shared/run/after/60-omz-update.sh: don't actually update omz
    Leave this to chezmoi

--- workspaces/shared/run/after/60-omz-update.sh
+++ workspaces/shared/run/after/60-omz-update.sh
@@ -6,8 +6,16 @@
 # Note, if you get a "fatal: not a git repository" error, see:
 # https://stackoverflow.com/a/33487142/197789
 #
-if test -d "${HOME}/.oh-my-zsh" ; then
-  # '-v minimal' surpresses some fancy output
-  zsh "${HOME}/.oh-my-zsh/tools/upgrade.sh" -v minimal
+if test true ; then
+  # Currently we use chezmoi to update omz rather than omz itself
+  # See .chezmoiexternal.toml
+  # If we try to mix the two, you get the git repo error above.
+  # XXX Not sure how to make this conditional
+  echo "Skipping oh-my-zsh update and letting chezmoi handle it."
+else
+  if test -d "${HOME}/.oh-my-zsh" ; then
+    # '-v minimal' surpresses some fancy output
+    zsh "${HOME}/.oh-my-zsh/tools/upgrade.sh" -v minimal
+  fi
 fi
 exit 0
