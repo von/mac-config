d2f295d386e6919e72e803924c85361b4c7632dd
Von Welch <von@vwelch.com>
2025-11-27 10:54:50 -0500

    new file:   workspaces/shared/run/after/92-vifm-trash-clean.sh

---
+++ workspaces/shared/run/after/92-vifm-trash-clean.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+# Remove any files in vifm trash not modified in last 30 days
+vifm_trash="/Users/von/.local/share/vifm/Trash"
+
+find "${vifm_trash}" -mtime +30d -type f -print -exec rm -f "{}" \;
+# And remove any empty directories
+# Kudos: http://www.thegeekstuff.com/2010/03/find-empty-directories-and-files/
+# The '-prune' prevents an error as find attempts to recurse into the
+# directory it just deleted.
+# '-mindepth 1' so we don't delete the Trash/ directory itself.
+find "${vifm_trash}" -type d -empty -mindepth 1 -print -exec rmdir "{}" \; -prune
+exit 0
