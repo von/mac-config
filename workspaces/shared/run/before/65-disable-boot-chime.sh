3d7f2d66e374b3f9b79cfbda6292c1e0f720a1b4
Von Welch <von@vwelch.com>
2026-01-18 12:25:09 -0500

    65-disable-boot-chime.sh: use 'nvram -p' instead of 'nvram -X'

--- workspaces/shared/run/before/65-disable-boot-chime.sh
+++ workspaces/shared/run/before/65-disable-boot-chime.sh
@@ -3,7 +3,7 @@
 # Kudos: https://osxdaily.com/2020/04/29/how-enable-startup-boot-sound-newer-mac/
 set -e
 
-nvram -X StartupMute | grep "0x01" > /dev/null
+nvram -p | grep StartupMute | grep "%01" > /dev/null
 if test $? -ne 0 ; then
   echo "Disable chime on boot"
   sudo nvram StartupMute=%01
